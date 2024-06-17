838c701df3e32a76f86b0091e73a482017add91d
Jose-Tchingunji-Inacio <josetchingunji@gmail.com>
2024-06-17 05:52:01 +0100

    eliminação do aqruivo AuthController e transferencia do codigo para o UsuarioController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,39 +10,5 @@ use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
-    public function showLoginForm()
-    {
-        return view('login');
-    }
 
-    public function login(Request $request)
-    {
-        $credentials = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
-        ]);
-    
-        Log::debug('Tentando login com:', ['email' => $credentials['email'], 'password' => $credentials['password']]);
-    
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-            return redirect()->intended('index');
-        }
-    
-        Log::debug('Falha na autenticação para o email:', ['email' => $credentials['email']]);
-    
-        return back()->withErrors([
-            'email' => 'As credenciais fornecidas não correspondem aos nossos registros.',
-        ]);
-    }
-
-    public function logout(Request $request)
-    {
-        Auth::logout();
-
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-
-        return redirect('/');
-    }
 }
